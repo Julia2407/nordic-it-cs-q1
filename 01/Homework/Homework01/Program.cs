3bf4536ac1bf01a416cd660a36472ab952456906
Julia <three_cats@mail.ru>
2019-10-31 17:28:22 +0300

    Homework1

---
+++ 01/Homework/Homework01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+namespace Homework01
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine($"Здравствуйте, {name}");
+            Thread.Sleep(5000);
+            Console.Write($"До свидания, {name}"); ;
+            Console.ReadKey();
+        }
+    }
+}
