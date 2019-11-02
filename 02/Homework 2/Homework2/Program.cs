c7fa597285f655d4011ba88718dd3816ec09c83f
Julia <three_cats@mail.ru>
2019-11-02 18:09:47 +0300

    Homework №2

---
+++ 02/Homework 2/Homework2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            string a = Console.ReadLine();
+            double x = double.Parse(a);
+            Console.WriteLine("Введите второе число");
+            string b = Console.ReadLine();
+            double y = double.Parse(b);
+            double res = x + y;
+            double res2 = Math.Abs(x - y);
+            double res3 = x * y;
+            Console.WriteLine($"Сумма = {res}");
+            Console.WriteLine($"Разность = {res2}");
+            Console.WriteLine($"Произведение = {res3}");
+            Console.ReadKey();
+        }
+    }
+}
