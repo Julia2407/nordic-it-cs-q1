3b802d60638f948cdaa8876958874717370e4a2a
Julia <three_cats@mail.ru>
2019-11-19 02:00:43 +0300

    the 2d part of Homework № 6

---
+++ 06_1/Homework № 6_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Homework___6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+            try
+            {
+                Console.WriteLine("Введите сумму первоначального взноса в рублях");
+                double contrb = double.Parse(Console.ReadLine());
+                Console.WriteLine(" Введите ежедневный процент дохода в виде десятичной дроби (1% = 0.01)");
+                double proc = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите желаемую сумму накопления в рублях");
+                int summ = int.Parse(Console.ReadLine());
+
+                int k = 0;
+                double c = contrb;
+                while (c < summ) 
+                {
+                    c = (c * proc) + c; 
+                    k++;
+                }
+                Console.WriteLine($"Необходимое количество дней для накопления желаемой суммы: {k}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введено неверное значение");
+                goto start;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Введен неверный формат");
+                goto start;
+
+            }
+              
+        }
+    }
+}
